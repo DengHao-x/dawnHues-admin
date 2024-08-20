80d6fb28180c7c5b8b3284d13194f71f628b04ca
denghao <2519864438@qq.com>
2024-08-20 19:43:57 +0800

    style: :lipstick: 格式化

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,57 +1,50 @@
-import {
-  type Router,
-  createRouter,
-  type RouteRecordRaw,
-  type RouteComponent,
-  createWebHashHistory
-} from 'vue-router'
-import NProgress from '@/utils/nprogress'
-import baseRoutes from './modules/baseRouter'
-import { getToken } from '@/utils/authentication'
-import { getAsyncRouter } from '@/api/asyncRouter'
+import { type Router, createRouter, type RouteRecordRaw, type RouteComponent, createWebHashHistory } from "vue-router";
+import NProgress from "@/utils/nprogress";
+import baseRoutes from "./modules/baseRouter";
+import { getToken } from "@/utils/authentication";
+import { getAsyncRouter } from "@/api/asyncRouter";
 /** 路由白名单 */
-const whiteList = ['/login']
+const whiteList = ["/login"];
 
 /** 创建路由实例 */
 export const router: Router = createRouter({
   history: createWebHashHistory(),
   routes: baseRoutes,
-  strict: true
-})
+  strict: true,
+});
 
 router.beforeEach((to, from, next) => {
-  NProgress.start()
+  NProgress.start();
   if (getToken()) {
-    getAsyncRouter().then(res => {
+    getAsyncRouter().then((res) => {
       const asyncRouter: RouteRecordRaw[] = res.data.map((item: any) => {
-        const component: RouteComponent = (() =>
-          import(`@/views/${item.component}.vue`)) as unknown as RouteComponent
+        const component: RouteComponent = (() => import(`@/views/${item.component}.vue`)) as unknown as RouteComponent;
         return {
           path: item.path,
           name: item.name,
           component,
           meta: {
             title: item.title,
-            icon: item.icon
-          }
-        }
-      })
-      console.log(asyncRouter)
-    })
-    next()
+            icon: item.icon,
+          },
+        };
+      });
+      console.log(asyncRouter);
+    });
+    next();
   } else {
-    if (to.path !== '/login') {
+    if (to.path !== "/login") {
       if (whiteList.indexOf(to.path) !== -1) {
-        next()
+        next();
       } else {
-        next({ path: '/login' })
+        next({ path: "/login" });
       }
     } else {
-      next()
+      next();
     }
   }
-})
+});
 router.afterEach(() => {
-  NProgress.done()
-})
-export default router
+  NProgress.done();
+});
+export default router;
